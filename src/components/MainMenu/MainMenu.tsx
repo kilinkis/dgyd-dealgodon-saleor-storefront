31d92dd484a14983b027ab764da49621891c3059
Juan Manuel Incaurgarat <juanofx@gmail.com>
2020-05-15 11:48:44 +0200

    Updated header to load img tag instead of svg

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -157,7 +157,8 @@ const MainMenu: React.FC = () => {
 
           <div className="main-menu__center">
             <Link to={appPaths.baseUrl}>
-              <ReactSVG path={logoImg} />
+              {/* <ReactSVG path={logoImg} /> */}
+              <img width="85" src={logoImg} />
             </Link>
           </div>
 
